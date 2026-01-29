5c367c8851494aca478025e7bff90f2f8fe6ad56
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-01-29 14:54:34 +0900

    added search function

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,4 +2,9 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+
+  def search
+    @search = params[:search]
+    @pokemons = Pokemon.where("name LIKE '%#{@search}%'")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   get "pokemons", to: "pokemons#index"
 
+  get "pokemons/search", to: "pokemons#search"
+
 end
