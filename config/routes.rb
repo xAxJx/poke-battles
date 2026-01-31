a19fd40878937c78f31f21b73155a03585632913
exec zsh <leticiamiwa5@gmail.com>
2026-01-31 14:15:04 +0900

    Add Devise authentication and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
 
   get "pokemons/search", to: "pokemons#search"
 
-  resources :moves, only[:index] do
+  resources :moves, only: [:index] do
 
   end
 
