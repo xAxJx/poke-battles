008d4eadb0339e174607d5f50f9839bd569d4875
Hisham <hisham.khashman@gmail.com>
2026-01-31 11:53:15 +0900

    teams model with dependant destroy

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :game
-  has_many :pokemons, dependent: :destroy
+  has_many :pokemons, dependent: :destroy #if game destroy then team destroy
 
   validates :opponent, presence: true
 end
