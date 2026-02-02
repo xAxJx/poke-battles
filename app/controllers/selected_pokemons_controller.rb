4ff2b5cfebf3ff1b6a0f902a439b00841d85ac01
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-02-02 16:49:20 +0900

    added team creation and pokemon select, but doesn't save pokemon properly

---
+++ app/controllers/selected_pokemons_controller.rb
@@ -0,0 +1,36 @@
+class SelectedPokemonsController < ApplicationController
+  def new
+    @selected_pokemon = SelectedPokemon.new
+    @selected_pokemon.pokemon_id = params[:pokemon_id]
+    @pokemon_list = Pokemon.all
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @team = Team.find(params[:team_id])
+    @game = Game.find(params[:game_id])
+  end
+
+  def create
+    @selected_pokemon = SelectedPokemon.new(selected_pokemon_params)
+    @selected_pokemon.pokemon_id = params[:pokemon_id]
+
+    @team = Team.find(params[:team_id])
+    @game = Game.find(params[:game_id])
+
+
+    if @selected_pokemon.save
+      redirect_to game_team_path(@game, @team)
+    else
+      render "new", status: :unprocessable_content # - but how do you pass the pokemon_id variable?
+      # new_game_team_selected_pokemon_path(@game, @team)
+    end
+
+  end
+
+  def show
+    @selected_pokemon = SelectedPokemon.find(params[:id])
+  end
+
+  private
+  def selected_pokemon_params
+    params.require(:selected_pokemon).permit(:pokemon_id, :team_id, :move1, :move2, :move3, :move4, :number, :game_id)
+  end
+end
---
+++ test/controllers/selected_pokemon_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SelectedPokemonControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
