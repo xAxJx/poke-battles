e8713a977c1520520ec0caccbfe39e4c4cf21dba
exec zsh <leticiamiwa5@gmail.com>
2026-02-03 22:28:08 +0900

    action model updated and action and battles db migrated

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,15 +1,16 @@
 class Action < ApplicationRecord
   belongs_to :battle
-  belongs_to :pokemon
+  belongs_to :selected_pokemon
   belongs_to :move
 
-  validates :battle, :pokemon, :move, presence: true
+  validates :battle, :selected_pokemon, :move, presence: true
   validate :pokemon_in_battle
 
   private
 
-  def pokemon_in_battle
-    unless battle.pokemons.include?(pokemon)
+  def selected_pokemon_in_battle
+    return if battle.nil? || selected_pokemon.nil?
+    unless selected_pokemon.game_id == battle.game_id
       errors.add(:pokemon, "not in the battle")
     end
   end
--- db/migrate/20260203131737_create_actions.rb
+++ db/migrate/20260203131737_create_actions.rb
@@ -2,7 +2,7 @@ class CreateActions < ActiveRecord::Migration[7.1]
   def change
     create_table :actions do |t|
       t.references :battle, null: false, foreign_key: true
-      t.references :pokemon, null: false, foreign_key: true
+      t.references :selected_pokemon, null: false, foreign_key: true
       t.references :move, null: false, foreign_key: true
 
       t.timestamps
