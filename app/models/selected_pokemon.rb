587806f6471c7342b611c78cac83a6bd9c9d4a38
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-02-05 20:13:09 +0900

    added team size limit

--- app/models/selected_pokemon.rb
+++ app/models/selected_pokemon.rb
@@ -3,13 +3,13 @@ class SelectedPokemon < ApplicationRecord
   belongs_to :pokemon
   has_many :moves, through: :learned_moves
 
-  # MAX_TEAM_SIZE = 3
+  MAX_TEAM_SIZE = 3
 
-  # validate :max_team_size
+  validate :max_team_size
 
-  # def max_team_size
-  #   if (SelectedPokemon.where((team_id == current_team_id)) >= MAX_TEAM_SIZE)
-  #     erros.add("Max team size is #{MAX_TEAM_SIZE} Pokemon.")
-  #   end
-  # end
+  def max_team_size
+    if (SelectedPokemon.where(team_id: team_id).size >= MAX_TEAM_SIZE)
+      errors.add("Max team size is #{MAX_TEAM_SIZE} Pokemon.")
+    end
+  end
 end
