35477eafab6eab12afa3777b38b4b1f32f42987a
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-02-05 23:52:15 +0900

    added opponent team generation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,7 +13,7 @@ class GamesController < ApplicationController
     @game = Game.new
     @game.user = current_user
     @game.status ||= "setup"  # Make sure the game starts in "setup" state
-    generate_pokemon
+
 
     if @game.save
       redirect_to game_path(@game)
@@ -25,6 +25,7 @@ class GamesController < ApplicationController
   def show
     @selected = SelectedPokemon.all
     @pokemons = Pokemon.all
+    @teams = Team.all
   end
 
   private
@@ -37,20 +38,6 @@ class GamesController < ApplicationController
   #   params.fetch(:game, {}).permit(:status)
   # end
   #
-  def generate_opponent
-    @game = Game.find(params[:id])
-    @newOpponent = Team.create!(game_id:@game.id, opponent:"opponent")
-    # @randPoke1 = Pokemon.find_by(number:rand(1..151))
-    @randPokemons = Pokemon.all.sample(3)
-    @randMoves1 = Move.all.sample(4)
-    @randMoves2 = Move.all.sample(4)
-    @randMoves3 = Move.all.sample(4)
-    @opponentPoke1 = SelectedPokemon.create!(team_id:@newOpponent.id, pokemon_id:@randPokemons[0].id, move1:@randMoves1[0].id, move2:@randMoves1[1].id, move3:@randMoves1[2].id, move4:@randMoves[3].id,)
-    @opponentPoke2 = SelectedPokemon.create!(team_id:@newOpponent.id, pokemon_id:@randPokemons[1].id, move1:@randMoves2[0].id, move2:@randMoves2[1].id, move3:@randMoves2[2].id, move4:@randMoves[3].id,)
-    @opponentPoke3 = SelectedPokemon.create!(team_id:@newOpponent.id, pokemon_id:@randPokemons[2].id, move1:@randMoves3[0].id, move2:@randMoves3[1].id, move3:@randMoves3[2].id, move4:@randMoves[3].id,)
-  end
 
-  def generate_opponent_pokemon
 
-  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -25,6 +25,7 @@ class TeamsController < ApplicationController
     @team.game = @game
     # set the opponent status to "player"
     @team.opponent = "player"
+    generate_opponent
 
     if @team.save
       # If everything is OK, go to the team page
@@ -68,4 +69,19 @@ class TeamsController < ApplicationController
   # def team_params
   #   params.require(:team).permit(:opponent)
   # end
+  #
+  private
+  def generate_opponent
+    @currentGame = Game.find(params[:game_id])
+    @newOpponent = Team.create!(game_id:@currentGame.id, opponent:"opponent")
+    # @randPoke1 = Pokemon.find_by(number:rand(1..151))
+    @randPokemons = Pokemon.all.sample(3)
+    @randMoves1 = Move.all.sample(4)
+    @randMoves2 = Move.all.sample(4)
+    @randMoves3 = Move.all.sample(4)
+    @opponentPoke1 = SelectedPokemon.create!(team_id:@newOpponent.id, pokemon_id:@randPokemons[0].id, move1:@randMoves1[0].id, move2:@randMoves1[1].id, move3:@randMoves1[2].id, move4:@randMoves1[3].id,)
+    @opponentPoke2 = SelectedPokemon.create!(team_id:@newOpponent.id, pokemon_id:@randPokemons[1].id, move1:@randMoves2[0].id, move2:@randMoves2[1].id, move3:@randMoves2[2].id, move4:@randMoves2[3].id,)
+    @opponentPoke3 = SelectedPokemon.create!(team_id:@newOpponent.id, pokemon_id:@randPokemons[2].id, move1:@randMoves3[0].id, move2:@randMoves3[1].id, move3:@randMoves3[2].id, move4:@randMoves3[3].id,)
+  end
+
 end
